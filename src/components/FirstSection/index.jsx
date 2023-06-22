0a6ef3800f39bdcffd4d582d89cb7a227fee0eb6
orvituhgo <orvituhgo@gmail.com>
2023-06-22 17:57:27 -0300

    change text intro

--- src/components/FirstSection/index.jsx
+++ src/components/FirstSection/index.jsx
@@ -6,7 +6,7 @@ export default function FirstSection() {
       <div className="home">
         <h1>Tenha um software eficiente e escalável.</h1>
         <p>
-          Sem blah blah blah técnico! Nós queremos te entregar resultados consistentes aumentando a produtividade do seu negócio através de uma consultoria de software que supere suas expectativas.
+          SemTESTESTETESETESNós queremos te entregar resultados consistentes aumentando a produtividade do seu negócio através de uma consultoria de software que supere suas expectativas.
         </p>
       </div>
       <div>
