6f36eddfc7adf5be66f5b83de2aa7a992facb4bc
orvituhgo <orvituhgo@gmail.com>
2023-06-22 22:50:24 -0300

    new testing fix on deploy

--- src/components/FirstSection/index.jsx
+++ src/components/FirstSection/index.jsx
@@ -7,9 +7,9 @@ export default function FirstSection() {
       <div className="home">
         <h1>Testando AMICO.</h1>
         <p>
-          Sdsadsadsdasdasico! Nós queremos te entregar resultados consistentes
-          aumentando a produtividade do seu negócio através de uma consultoria
-          de software que supere suas expectativas.
+          um dolor sit amet consectetur, adipisicing elit. Molestiae, repellat
+          aut aliquam non nam veritatis impedit consectetur! Odit illum qavés
+          de uma consultoria de software que supere suas expectativas.
         </p>
       </div>
       <div>
--- src/configs/light.js
+++ src/configs/light.js
@@ -1,5 +1,7 @@
-export const primaryColor = '#1a1a1d';
-export const secondaryColor = '#3C37F1';
+// export const primaryColor = '#1a1a1d';
+export const primaryColor = '#1648DE';
+// export const secondaryColor = '#3C37F1';
+export const secondaryColor = '#2724F5';
 export const bgColor = '#eee';
 export const text = '#fff';
 
